e6ba03f41cf6f9ed43aa77145668367e7e1d7082
chonburee01 <chonburee01@gmail.com>
2026-05-21 16:48:53 +0700

    first commit

---
+++ visualstudio_run_as_admin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace visualstudio_run_as_admin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            string slnPath = args[0];
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe",
+                Arguments = "\"" + slnPath + "\"",
+                Verb = "runas",
+                UseShellExecute = true
+            };
+
+            Process.Start(psi);
+        }
+    }
+}
